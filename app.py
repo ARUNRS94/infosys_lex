318041e297c7c8c21980494ba6806988006967c0
Arun RS <31178150+ARUNRS94@users.noreply.github.com>
2023-06-04 18:56:38 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, jsonify
+import pyperclip
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/post', methods=['POST'])
+def post_data():
+    data = request.get_json()
+    clipboard_data = data['data']
+    soup = BeautifulSoup(clipboard_data, 'html.parser')
+
+    # Find all elements with class="options word-break figure-image" and id="problemStatement"
+    options = soup.find_all('div', {'class': 'options word-break figure-image'})
+    options_text = [o.text for o in options]
+    problem_statement = soup.find('div', {'id': 'problemStatement'})
+    text = problem_statement.get_text()
+
+    # Print the filtered elements
+    #print(text)
+    #print("Choose the best one with out explanation")
+    # print(options_text)
+    response = text
+    response += "\n" + "Choose the best one with out explanation"
+    #print(response)
+    for count, i in enumerate(options_text):
+        #print(count, i)
+        #print(options_text)
+        option_no = count + 1
+        response += "\n" + str(option_no) + "." + i
+        print(response)
+
+    #response = "This is the server response"
+
+    # Copy response to clipboard
+    pyperclip.copy(response)
+
+    return jsonify({'response': response})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
